294658ceb72a57a358c03ee6b494e42e25f5ae81
Mahmoud Adel <mahmoudadel200190@yahoo.com>
2024-05-06 22:25:49 +0300

    glad imported

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 int main(void) {
     GLFWwindow* window;
@@ -20,7 +21,7 @@ int main(void) {
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
