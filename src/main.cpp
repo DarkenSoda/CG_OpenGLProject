1e86d9603fb0ff658c4e1ddb3ec47c8c56c06edb
Mahmoud Adel <mahmoudadel200190@yahoo.com>
2024-05-07 04:21:19 +0300

    Creating a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <iostream>
+
+struct MousePosition {
+    double x, y;
+};
+
+void processInput(GLFWwindow* window);
+double calculateFPS(GLFWwindow* window);
+MousePosition cursorPosition(GLFWwindow* window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void updateTitle(GLFWwindow* window, const char* title, double fps, MousePosition cursorPos);
 
 int main(void) {
     GLFWwindow* window;
+    char title[] = "OpenGL Project";
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, title, NULL, NULL);
     if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -18,18 +36,77 @@ int main(void) {
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // gladLoadGL();
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 640, 480);
+
+    // window resize callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+    printf("Refresh Rate: %dHz\n", glfwGetVideoMode(glfwGetPrimaryMonitor())->refreshRate);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
+        // Close window on ESC key press
+        processInput(window);
+
+        // update window title with FPS and cursor position
+        updateTitle(window, title, calculateFPS(window), cursorPosition(window));
+
         /* Render here */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
         /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+double calculateFPS(GLFWwindow* window) {
+    static double fps;
+    static double previousTime = glfwGetTime();
+    static int frameCount = 0;
+    double currentTime = glfwGetTime();
+    frameCount++;
+
+    if (currentTime - previousTime >= 1.0) {
+        fps = frameCount / (currentTime - previousTime);
+        frameCount = 0;
+        previousTime = currentTime;
+    }
+
+    return fps;
+}
+
+MousePosition cursorPosition(GLFWwindow* window) {
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+
+    // Update window title with cursor position
+    return { xpos, ypos };
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void updateTitle(GLFWwindow* window, const char* title, double fps, MousePosition cursorPos) {
+    char newTitle[256];
+    sprintf(newTitle, "%s | FPS: %.1f | Cursor Position: %.2f, %.2f", title, fps, cursorPos.x, cursorPos.y);
+    glfwSetWindowTitle(window, newTitle);
 }
